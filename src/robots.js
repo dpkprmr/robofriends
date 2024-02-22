ab3b4e96ab9fa349f540b04ca71422679ff760dd
Deepak Parmar <parmar.deepak82@gmail.com>
2024-02-22 17:22:06 +0530

    Created the Robofriends React App by Learning React from ZTM

---
+++ src/robots.js
@@ -0,0 +1,81 @@
+export const robots = [
+    {
+        id: 1,
+        uuid: "fea9d62e-de31-4d27-860a-aa53673e4678",
+        name: "Darel Harvey",
+        username: "Roshaunda",
+        birthDate: "23 Apr 1980",
+        email: "jacquie_adamesi7@reprints.ic"
+    },
+    {
+        id: 2,
+        uuid: "0679b358-3d46-4441-a6e2-d1f0b7d79db3",
+        name: "Jourdan Morin",
+        username: "Shanessa",
+        birthDate: "17 Aug 1986",
+        email: "etosha_baudoinfsr@packaging.ok"
+    },
+    {
+        id: 3,
+        uuid: "b6222100-9a64-4da1-a4db-673567897c21",
+        name: "Margrett Grieve",
+        username: "Shada",
+        birthDate: "31 Mar 2012",
+        email: "feliza_blakelyt@genuine.vqm"
+    },
+    {
+        id: 4,
+        uuid: "2f6e406a-9ca2-44f9-9e92-e576ce48462f",
+        name: "Kruti Romer",
+        username: "Roxan",
+        birthDate: "01 Jan 1990",
+        email: "hoai_plunkett4n@resistant.psx"
+    },
+    {
+        id: 5,
+        uuid: "e2301d64-cf7e-44a1-b718-e99d228a397e",
+        name: "Annalynn Toll",
+        username: "Fredric",
+        birthDate: "30 Dec 1992",
+        email: "tashonna_robitaille7sun@brazil.zy"
+    },
+    {
+        id: 6,
+        uuid: "d339ffca-a31c-4e67-ae0d-3f54a8088ef9",
+        name: "Katoya Cardinal",
+        username: "Gibson",
+        birthDate: "09 Sep 1982",
+        email: "donnielle_brase7uv@backgrounds.pt"
+    },
+    {
+        id: 7,
+        uuid: "de179a4f-a510-4fb1-8798-5b05b6281218",
+        name: "Markell Maffei",
+        username: "Ugo",
+        birthDate: "26 May 1970",
+        email: "migdalia_wertzog@employment.zjv"
+    },
+    {
+        id: 8,
+        uuid: "899a9d75-cd83-499c-a984-d2c22f0f1d2f",
+        name: "Margret Felt",
+        username: "Teagan",
+        birthDate: "24 Jul 2013",
+        email: "endia_aguilar3n@endorsement.wn"
+    },
+    {
+        id: 9,
+        uuid: "1dd5d98a-e7c7-4b20-9aa8-aa17a00434bc",
+        name: "Dontavious Geib",
+        username: "Barett",
+        birthDate: "06 Jun 1997",
+        email: "taleisha_nelsonbu3@possible.hud"
+    },
+    {
+        id: 10,
+        uuid: "15210537-6607-4387-8c01-eff1f46e26e7",
+        name: "Reshonda Mallet",
+        username: "Avital",
+        birthDate: "26 Jul 1974",
+        email: "kimiko_carillov@interim.xxj"
+    }]
